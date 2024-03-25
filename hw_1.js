22421d393f202da81da3b324c02044ac2adc69c0
ESKGH <evgenij.kovalev.1999@mail.ru>
2024-03-25 15:42:52 +0300

    homework 2 added

--- hw_1.js
+++ hw_1.js
@@ -1,1 +1,80 @@
-console.log('Hello World!')
+
+let x = 20
+let y = 58
+let z = 42
+
+let sum = x + y + z
+console.log(sum);
+
+let seconds = 1
+let minute = 60 * seconds
+let hour = 60 * minute
+let day = 24 * hour
+let year = 365 * day
+
+myAgeInSeconds = 23 * year 
+console.log(myAgeInSeconds);
+
+let count = 42
+console.log(count)
+String(count)
+console.log(count)
+count.toString( )
+console.log(count)
+let userName = '42'
+console.log(userName)
+parseInt(userName)
+console.log(userName)
+Number(userName)
+console.log(userName)
+
+
+let a = 1
+let b = 2
+let c = 'белых медведей'
+
+let text = a.toString() + b.toString() + ' ' + c
+console.log(text)
+
+
+
+let word1 = "морпех"
+let word2 = "наледь"
+let word3 = "попрек"
+let word4 = "рубило"
+
+let lengthWord1 = word1.length;
+let lengthWord2 = word2.length;
+let lengthWord3 = word3.length;
+let lengthWord4 = word4.length;
+
+let lengthWords = lengthWord1 + lengthWord2 + lengthWord3 + lengthWord4
+console.log(lengthWords)
+
+
+
+var stringVariable = "Privet!";
+var numberVariable = 42;
+var booleanVariable = true;
+
+console.log("Variable: stringVariable have type: " + typeof stringVariable);
+console.log("Variable: numberVariable have type: " + typeof numberVariable);
+console.log("Variable: booleanVariable have type: " + typeof booleanVariable);
+
+
+
+let a1 = 4;
+let b1 = 3;
+
+console.log("a1 = ", b1)
+console.log("b1 = ", a1)
+
+
+let codeWord1 = "обернись";
+let codeWord2 = "неужели";
+let codeWord3 = "огурцы";
+let codeWord4 = "липкие";
+let codeWord5 = "?!";
+
+let cipher = codeWord1[1] + codeWord2 [1] +codeWord3 [1] + codeWord4 [1] + codeWord5 [1]
+console.log(cipher)
