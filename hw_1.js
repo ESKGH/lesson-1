783f0302574f326abc4ba8cd2052a74ba0998621
ESKGH <evgenij.kovalev.1999@mail.ru>
2024-03-25 16:55:28 +0300

    exercise 7 added

--- hw_1.js
+++ hw_1.js
@@ -2,9 +2,9 @@
 let x = 20
 let y = 58
 let z = 42
-
 let sum = x + y + z
 console.log(sum);
+
 
 let seconds = 1
 let minute = 60 * seconds
@@ -16,25 +16,22 @@ myAgeInSeconds = 23 * year
 console.log(myAgeInSeconds);
 
 let count = 42
-console.log(count)
-String(count)
-console.log(count)
-count.toString( )
-console.log(count)
+let count_String = String(count)
+console.log(typeof count_String + count_String)
+let count_String_2 = count.toString()
+console.log(typeof count_String_2 + count_String_2)
 let userName = '42'
-console.log(userName)
-parseInt(userName)
-console.log(userName)
-Number(userName)
-console.log(userName)
-
+let userName_Number = Number(userName)
+console.log(typeof userName_Number + userName_Number)
+let userName_Number_2 = BigInt(userName)
+console.log(typeof userName_Number_2 + userName_Number_2);
 
 let a = 1
 let b = 2
 let c = 'белых медведей'
 
 let text = a.toString() + b.toString() + ' ' + c
-console.log(text)
+console.log(text);
 
 
 
@@ -49,32 +46,48 @@ let lengthWord3 = word3.length;
 let lengthWord4 = word4.length;
 
 let lengthWords = lengthWord1 + lengthWord2 + lengthWord3 + lengthWord4
-console.log(lengthWords)
+console.log(lengthWords);
 
 
 
-var stringVariable = "Privet!";
-var numberVariable = 42;
-var booleanVariable = true;
+let stringVariable = "Privet!"
+let numberVariable = 42
+let booleanVariable = true
 
-console.log("Variable: %stringVariable% have type: " + "%" + typeof stringVariable + "%");
-console.log("Variable: %numberVariable% have type: " + "%" + typeof numberVariable + "%");
-console.log("Variable: %booleanVariable% have type: " + "%" + typeof booleanVariable + "%");
-
-
-
-let a1 = 4;
-let b1 = 3;
-
-console.log("a1 = ", b1)
-console.log("b1 = ", a1)
+console.log(`Variable: %stringVariable% have type: %${typeof stringVariable}%`)
+console.log(`Variable: %numberVariable% have type: %${typeof numberVariable}%`)
+console.log(`Variable: %booleanVariable% have type: %${typeof booleanVariable}%`);
 
 
-let codeWord1 = "обернись";
-let codeWord2 = "неужели";
-let codeWord3 = "огурцы";
-let codeWord4 = "липкие";
-let codeWord5 = "?!";
+
+
+
+
+
+let a1 = 4
+let b1 = 3
+a1 = a1 + b1
+b1 = a1 - b1
+a1 = a1 - b1
+
+console.log("a1 =", a1) 
+console.log("b1 =", b1); 
+
+
+
+let codeWord1 = "обернись"
+let codeWord2 = "неужели"
+let codeWord3 = "огурцы"
+let codeWord4 = "липкие"
+let codeWord5 = "?!"
 
 let cipher = codeWord1[1] + codeWord2 [1] +codeWord3 [1] + codeWord4 [1] + codeWord5 [1]
-console.log(cipher)
+console.log(cipher);
+
+
+const NewUsername = prompt("Пожалуйста, введите ваше имя:")
+const NewUserage = prompt("Пожалуйста, введите ваш возраст:")
+
+console.log("Имя пользователя: " + NewUsername)
+console.log("Возраст пользователя: " + NewUserage);
+
